7353a7c3e2a4f6096015e27b8d5a5972424614da
Mdsoln <muddyfakih98@gmail.com>
2024-08-04 16:57:12 +0300

    customized component, binding of data and ng events handling

---
+++ src/app/advertise-page/advertise-page.component.ts
@@ -0,0 +1,34 @@
+import { NgFor, NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-advertise-page',
+  standalone: true,
+  imports: [FormsModule, NgIf, NgFor],
+  templateUrl: './advertise-page.component.html',
+  styleUrl: './advertise-page.component.scss'
+})
+export class AdvertisePageComponent {
+
+  name: string;
+  email: string;
+  message: string;
+  isSubmitted: boolean = false;
+  tableMessage: Array<any> = [];
+
+  constructor() {
+    this.name = '';
+    this.email = '';
+    this.message = '';
+  }
+
+  onSubmit(){
+    this.isSubmitted = true;
+    this.tableMessage.push({
+      'name': this.name,
+      'email': this.email,
+      'message': this.message
+    })
+  }
+}
